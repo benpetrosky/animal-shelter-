36532fe748b679aa88e331f2dc37a14d918ceffb
Ben Petrosky and Dan Jacob <ben+dan@>
2017-05-02 17:23:37 -0700

    add experimental routing in app.rb, issue fetching from form and running through animal class.

--- app.rb
+++ app.rb
@@ -11,13 +11,21 @@ get("/") do
   erb(:index)
 end
 
+
+
+get('/add_animal_form/new') do
+  @animals = Animal.all()
+  erb(:add_animal_form)
+end
+
 post("/animals") do
   animal_name = params.fetch('animal_name')
   animal_gender = params.fetch('animal_gender')
   animal_arrival_date = params.fetch('animal_arrival_date')
   type = params.fetch('type')
   breed = params.fetch("breed")
-  animal = Animal.new({:animal_name => animal_name,:animal_arrival_date => animal_arrival_date, :type => type, :breed => breed, :animal_gender => animal_gender})
-  animal.save()
+  @animal = Animal.new({:animal_name => animal_name, :list_id => nil, :animal_arrival_date => animal_arrival_date, :type => type, :breed => breed, :animal_gender => animal_gender})
+  @animal.save()
+  @animals = Animal.all()
   erb(:success)
 end
